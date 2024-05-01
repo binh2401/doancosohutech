f439089bc5dac27fdbaa54e7be875b5eacc9af48
binh2401 <thanhbinh7a2bauham@gmail.com>
2024-05-01 18:08:49 +0700

    1

---
+++ WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // Hi?n th? danh sách s?n ph?m
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
