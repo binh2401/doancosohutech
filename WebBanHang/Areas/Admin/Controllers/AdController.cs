1a1f36fa3bbb2999879311802dc37a5df0154f1b
SeamussK <giakiettruong5@gmail.com>
2024-05-07 18:12:51 +0700

    admin

---
+++ WebBanHang/Areas/Admin/Controllers/AdController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class AdController : Controller
+    {
+        [Area("Admin")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
